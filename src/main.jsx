09aa15a17dfbc01747bbcd26d64d428d20390575
seunggyu <devkim0702@gmail.com>
2024-03-13 23:38:09 +0900

    Feat: complete initial routings with react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./components/App";
+import ErrorPage from "./components/ErrorPage";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
