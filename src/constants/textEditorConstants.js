a2b9956e5e332d3ab7434611c6aa1f48851fbda9
seunggyu <devkim0702@gmail.com>
2024-03-14 00:31:32 +0900

    Feat: create textEditorConstants file

---
+++ src/constants/textEditorConstants.js
@@ -0,0 +1,12 @@
+export const COLORS = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "purple",
+  "pink",
+  "black",
+  "white",
+];
+export const FONT_SIZES = ["Small", "Medium", "Large", "X-Large"];
