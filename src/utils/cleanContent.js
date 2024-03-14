65bb4410e1ecca62081a3b647e52e83788343f19
seunggyu <devkim0702@gmail.com>
2024-03-14 16:24:08 +0900

    Fix: modify cleanContent util function

--- src/utils/cleanContent.js
+++ src/utils/cleanContent.js
@@ -1,27 +1,47 @@
 export function addIndent(htmlContent) {
   const parser = new DOMParser();
   const doc = parser.parseFromString(htmlContent, "text/html");
+  const alignClasses = ["text-left", "text-center", "text-right"];
+  const listTypes = ["list-decimal", "list-disc"];
+  const paddingClasses = ["pl-[40px]"];
 
-  doc.querySelectorAll("ol").forEach((ol) => {
-    ol.classList.add("list-decimal", "pl-[40px]");
-  });
+  function removeAllAlignClasses(element) {
+    alignClasses.forEach((el) => element.classList.remove(el));
+  }
 
-  doc.querySelectorAll("ul").forEach((ul) => {
-    ul.classList.add("list-disc", "pl-[40px]");
+  function removeAllListClasses(element) {
+    listTypes.forEach((el) => element.classList.remove(el));
+    paddingClasses.forEach((el) => element.classList.remove(el));
+  }
+
+  doc.querySelectorAll("ol, ul").forEach((list) => {
+    removeAllListClasses(list);
+
+    if (list.tagName.toLowerCase() === "ol") {
+      list.classList.add("list-decimal", "pl-[40px]");
+    } else if (list.tagName.toLowerCase() === "ul") {
+      list.classList.add("list-disc", "pl-[40px]");
+    }
   });
 
   doc.querySelectorAll('[style*="text-align: left"]').forEach((el) => {
+    removeAllAlignClasses(el);
+
     el.classList.add("text-left");
     el.removeAttribute("style");
   });
 
   doc.querySelectorAll('[style*="text-align: center"]').forEach((el) => {
+    removeAllAlignClasses(el);
     el.classList.add("text-center");
+
     el.removeAttribute("style");
   });
 
   doc.querySelectorAll('[style*="text-align: right"]').forEach((el) => {
+    removeAllAlignClasses(el);
     el.classList.add("text-right");
+
     el.removeAttribute("style");
   });
 
