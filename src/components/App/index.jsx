7eed64ec170b88a643186334f500993cda6c8533
seunggyu <devkim0702@gmail.com>
2024-03-14 13:23:09 +0900

    Feat: add ArticleDetailPage route for App component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,6 +3,7 @@ import Header from "../Header";
 import MainPage from "../MainPage";
 import ErrorPage from "../ErrorPage";
 import NewArticle from "../NewArticle";
+import ArticleDetailPage from "../ArticleDetailPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" exact element={<MainPage />} />
         <Route path="/articles/new" element={<NewArticle />} />
+        <Route path="/articles/:articleId" element={<ArticleDetailPage />} />
         <Route path="*" element={<ErrorPage errorMessage="404 Not Found!" />} />
       </Routes>
     </>
