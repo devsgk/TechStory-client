f62eb409be47f74b124d3adbb5512ba123b6d6df
seunggyu <devkim0702@gmail.com>
2024-03-14 12:55:41 +0900

    Feat: create checkUserIdentity util function

--- src/utils/checkPermission.js
+++ src/utils/checkPermission.js
@@ -11,3 +11,30 @@ export async function checkLoggedInStatus() {
 
   return userData;
 }
+
+export async function checkUserIdentity(articleId, userData) {
+  const response = await axios.get(
+    `${import.meta.env.VITE_BASE_URL}/articles?articleId=${articleId}`,
+    {
+      withCredentials: true,
+      articleId,
+    },
+  );
+
+  const data = response.data;
+  const isReviewer = data.article.reviewers.some(
+    (el) => el.user === userData._id,
+  );
+  const isAuthor = data.article.author === userData._id;
+  let identity;
+
+  if (isReviewer) {
+    identity = "reviewer";
+  } else if (isAuthor) {
+    identity = "author";
+  } else {
+    identity = "unAuthorized";
+  }
+
+  return identity;
+}
