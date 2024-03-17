60c9cf9a0baf0412203e03e1f6ba8c1603b6134e
seunggyu <devkim0702@gmail.com>
2024-03-17 18:16:03 +0900

    Feat: complete ReviewFormContainer component

---
+++ src/components/ReviewFormContainer/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ReviewFormContainer = React.forwardRef(({ children, position }, ref) => {
+  const containerStyle = {
+    top: `${position.y + 15}px`,
+    right: "30px",
+    position: "absolute",
+    zIndex: 1001,
+  };
+
+  return (
+    <div ref={ref} style={containerStyle}>
+      {children}
+    </div>
+  );
+});
+
+export default ReviewFormContainer;
