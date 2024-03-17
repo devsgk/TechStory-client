faac6095b9971cd922effd24f25cf732078e75f6
seunggyu <devkim0702@gmail.com>
2024-03-17 19:28:26 +0900

    Feat: complete MainPage component

---
+++ src/components/MainPage/index.jsx
@@ -0,0 +1,48 @@
+import { useNavigate } from "react-router-dom";
+
+import ArticlePreview from "../ArticlePreview";
+import { useRef } from "react";
+
+export default function MainPage() {
+  const navigate = useNavigate();
+  const mainRef = useRef(null);
+
+  function handleClick() {
+    if (mainRef.current) {
+      const elementTopPosition =
+        mainRef.current.getBoundingClientRect().top + window.scrollY;
+      const offsetPosition = elementTopPosition - 100;
+
+      window.scrollTo({ top: offsetPosition, behavior: "smooth" });
+    }
+  }
+
+  function handlePreviewClick(el) {
+    navigate(`/articles/${el._id}/published`);
+  }
+  return (
+    <>
+      <div
+        className="flex flex-col justify-center pl-7 bg-[#ffbf16]
+    border-b-[1px] border-black"
+      >
+        <h1 className="mt-[100px] mb-[20px] text-7xl">
+          <p>Stay hungry.</p>
+        </h1>
+        <div className="mt-3 text-2xl w-[450px]">
+          Explore stories, insights, and experiences from experts in tech
+          industry.
+        </div>
+        <button
+          className="rounded-full mt-10 mb-[100px] mr-auto text-xl font-light py-2 px-12 bg-black text-white"
+          onClick={handleClick}
+        >
+          Start reading
+        </button>
+      </div>
+      <div ref={mainRef}>
+        <ArticlePreview onClick={handlePreviewClick} />
+      </div>
+    </>
+  );
+}
