e8f6e958ce1f9ce3d83c286924235c8f3032d39d
seunggyu <devkim0702@gmail.com>
2024-03-17 18:41:37 +0900

    Feat: add commentStyleConstants

---
+++ src/constants/commentStyleConstants.js
@@ -0,0 +1,9 @@
+const COMMENT_STYLE = {
+  textDecorationLine: "underline",
+  textDecorationColor: "#FCD12A",
+  backgroundColor: "#FFFDD0",
+  textUnderlineOffset: "5px",
+  textDecorationThickness: "1.5px",
+};
+
+export default COMMENT_STYLE;
