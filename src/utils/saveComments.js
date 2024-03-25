cf409bb9b8e36aa4eedae1a4df26446b6f3db2fd
seunggyu <devkim0702@gmail.com>
2024-03-25 14:11:44 +0900

    Feat: create saveComments util function

---
+++ src/utils/saveComments.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export default async function saveComment(
+  articleContent,
+  articleId,
+  commentObj,
+) {
+  await axios.post(
+    `${import.meta.env.VITE_BASE_URL}/articles/${articleId}/review`,
+    {
+      withCredentials: true,
+      articleContent,
+      articleId,
+      commentObj,
+    },
+  );
+}
